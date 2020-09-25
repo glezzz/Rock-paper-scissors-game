40fa1212280dde85c1333f4ecaa7ac45ef22d57a
glezzz <alexglezk@gmail.com>
2020-09-25 12:57:31 +0200

    refactoring code

--- script.js
+++ script.js
@@ -1,80 +1,108 @@
 
 // get user choice
-document.getElementById("rock").addEventListener("click", function () {
-    userChoice = "rock";
-    playGame()
 
-})
-document.getElementById("paper").addEventListener("click", function () {
-    userChoice = "paper";
-    playGame()
+function playOption (event) {
+    playGame(event.target.getAttribute("id"))
 
-})
-document.getElementById("scissors").addEventListener("click", function () {
-    userChoice = "scissors";
-    playGame()
+}
 
-})
+document.getElementById("rock").addEventListener("click", playOption);
+document.getElementById("paper").addEventListener("click", playOption);
+document.getElementById("scissors").addEventListener("click", playOption);
 
-let userScore = 1;
-let compScore = 1;
+let userScore = 0;
+let compScore = 0;
+
+function reset() {
+    userScore = 0;
+    compScore = 0;
+
+
+    let userScoreElement = document.getElementById("user-score");
+    userScoreElement.innerHTML = userScore;
+
+    let computerScoreElement = document.getElementById("computer-score");
+    computerScoreElement.innerHTML = compScore;
+
+    let computerChoiceElement = document.getElementById("computer-choice");
+    computerChoiceElement.innerHTML = "Computer picks";
+
+
+    let result = document.getElementById("result");
+    result.innerHTML = "Make your move"
+}
+
+document.getElementById("reset-btn").addEventListener("click", reset);
 
 // get computer choice & play game
-function playGame() {
+function playGame(userChoice) {
 
     // get computer choice
     let computerChoice = Math.random(); // you can also do *3 and have 1,2 & 3 in the if stments
 
+    let computerChoiceElement = document.getElementById("computer-choice");
+
     if (computerChoice < 0.34) {
         computerChoice = "rock";
-        document.getElementById("computer-choice").innerHTML = "Computer picks rock";
 
     } else if (computerChoice <= 0.67) {
         computerChoice = "paper";
-        document.getElementById("computer-choice").innerHTML = "Computer picks paper";
 
     } else {
         computerChoice = "scissors";
-        document.getElementById("computer-choice").innerHTML = "Computer picks scissors";
     }
+    computerChoiceElement.innerHTML = "Computer picks " + computerChoice;
 
-    //play game
+
+    // calculate game outcome
+    // First make the calcs and then display the results
+
+    let result = document.getElementById("result");
+    let userScoreElement = document.getElementById("user-score");
+    let computerScoreElement = document.getElementById("computer-score");
+
+    //play game: -1 = computer wins, 0 = draw, 1 = user wins
+    const USER_WIN = 1;
+    let outcome;
+
     if (userChoice === computerChoice) {
-        document.getElementById("result").innerHTML = "It's a draw";
+        outcome = 0;
     }
     if (userChoice === "rock") {
         if (computerChoice === "scissors") {
-            document.getElementById("result").innerHTML = "You pick rock, you win";
-            document.getElementById("user-score").innerHTML = userScore++;
+            outcome = USER_WIN;
 
         } else if (computerChoice === "paper") {
-            document.getElementById("result").innerHTML = "You pick rock, you lose";
-            document.getElementById("computer-score").innerHTML = compScore++;
-
+            outcome = -1;
         }
-    }
-    if (userChoice === "paper") {
+    } else if (userChoice === "paper") {
         if (computerChoice === "scissors") {
-            document.getElementById("result").innerHTML = "You pick paper, you lose";
-            document.getElementById("computer-score").innerHTML = compScore++;
+            outcome = -1;
 
         } else if (computerChoice === "rock") {
-            document.getElementById("result").innerHTML = "You pick paper, you win";
-            document.getElementById("user-score").innerHTML = userScore++;
-
+            outcome = USER_WIN;
         }
-    }
-    if (userChoice === "scissors") {
+    } else if (userChoice === "scissors") {
         if (computerChoice === "rock") {
-            document.getElementById("result").innerHTML = "You pick scissors, you lose";
-            document.getElementById("computer-score").innerHTML = compScore++;
+            outcome = -1;
 
         } else if (computerChoice === "paper") {
-            document.getElementById("result").innerHTML = "You pick scissors, you win";
-            document.getElementById("user-score").innerHTML = userScore++;
-
+            outcome = USER_WIN;
         }
+    }
 
+    switch(outcome) {
+        case -1: // lose
+            result.innerHTML = "You pick " + userChoice + ", you lose";
+            computerScoreElement.innerHTML = ++compScore;
+            break;
+        case 0: // draw
+            result.innerHTML = "It's a draw";
+            break;
+        case USER_WIN: // win
+            result.innerHTML = "You pick " + userChoice + ", you win";
+            userScoreElement.innerHTML = ++userScore; // ++ a la izquierda cambia el valor ANTES de usarlo, ala derecha DESPUES
+            break;
     }
 }
 
